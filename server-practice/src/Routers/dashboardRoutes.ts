b3b830bd866b1e4c34ccb6e5f8a6a38adc3ef669
Mathew Cook <cook6348@gmail.com>
2025-03-10 07:51:08 -0500

    feat: restructure and typing files

--- server-practice/src/Routers/dashboardRoutes.ts
+++ server-practice/src/Routers/dashboardRoutes.ts
@@ -1,27 +1,26 @@
-
-import express from 'express';
+import express, { Request, Response } from 'express';
 const router = express.Router();
 
 // GET route for fetching dashboard data
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.send('Fetching dashboard data...');
 });
 
 // POST route for creating new dashboard data
-router.post('/', (req, res) => {
+router.post('/', (req: Request, res: Response) => {
   const newData = req.body;
   res.send(`Creating new dashboard data: ${JSON.stringify(newData)}`);
 });
 
 // PUT route for updating existing dashboard data
-router.put('/:id', (req, res) => {
+router.put('/:id', (req: Request, res: Response) => {
   const id = req.params.id;
   const updatedData = req.body;
   res.send(`Updating dashboard data with ID ${id}: ${JSON.stringify(updatedData)}`);
 });
 
 // DELETE route for deleting dashboard data
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req: Request, res: Response) => {
   const id = req.params.id;
   res.send(`Deleting dashboard data with ID ${id}`);
 });
